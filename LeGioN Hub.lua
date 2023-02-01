e1a41a480a1b0c9b6c9a469acf0fa7a607fdc92d
LeGioNPsyQ <65556998+LeGioNPsyQ@users.noreply.github.com>
2023-02-01 14:18:42 +0100

    Update LeGioN Hub.lua

--- LeGioN Hub.lua
+++ LeGioN Hub.lua
@@ -1,9 +1,9 @@
 local Games = {
-	[10404327868] = "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/Timber%20Champions.lua", -- Timber Champions
-	[10821317529] = "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/Pickaxe%20Mining%20Simulator.lua", -- Pickaxe Mining Simulator
-	[9264596435] = "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/Idle%20Heroes%20Simulator.lua", -- Idle Heroes Simulator
-	[10925589760] = "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/Merge%20Simulator.lua", -- Merge Simulator
-	[11445923563] = "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/One%20Fruit%20Simulator.lua" -- One Fruit Simulator
+	[10404327868] = "https://raw.githubusercontent.com/LeGioNPsyQ//LeGioN-Hub/main/Games/Timber%20Champions.lua", -- Timber Champions
+	[10821317529] = "https://raw.githubusercontent.com/LeGioNPsyQ//LeGioN-Hub/main/Games/Pickaxe%20Mining%20Simulator.lua", -- Pickaxe Mining Simulator
+	[9264596435] = "https://raw.githubusercontent.com/LeGioNPsyQ//LeGioN-Hub/main/Games/Idle%20Heroes%20Simulator.lua", -- Idle Heroes Simulator
+	[10925589760] = "https://raw.githubusercontent.com/LeGioNPsyQ//LeGioN-Hub/main/Games/Merge%20Simulator.lua", -- Merge Simulator
+	[11445923563] = "https://raw.githubusercontent.com/LeGioNPsyQ//LeGioN-Hub/main/Games/One%20Fruit%20Simulator.lua" -- One Fruit Simulator
 }
 
 if Games[game.PlaceId] then
