ccd5066d91791fc976224a381b2eff590c3dc8f6
LeGioNPsyQ <65556998+LeGioNPsyQ@users.noreply.github.com>
2023-02-02 04:39:24 +0100

    Update Pickaxe Mining Simulator.lua

--- Games/Pickaxe Mining Simulator.lua
+++ Games/Pickaxe Mining Simulator.lua
@@ -239,10 +239,10 @@ Pets:CreateToggle({
 	Callback = function(Value)	end,
 })
 
-Player.PlayerGui.Main.Prompts.Inventory.ScrollingFrame.ChildAdded:Connect(function(Child)
+Player.PlayerGui.Main.Prompts.InventoryService.ScrollingFrame.ChildAdded:Connect(function(Child)
 	if Rayfield.Flags.BestPick.CurrentValue and Child.ClassName == "ImageButton" then
 		ChildAdded = true
-		firesignal(Player.PlayerGui.Main.Prompts.Inventory.EquipBest.Activated)
+		firesignal(Player.PlayerGui.Main.Prompts.InventoryService.EquipBest.Activated)
 	end
 end)
 
