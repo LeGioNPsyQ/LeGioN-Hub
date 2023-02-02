d10f8a6321a9aa7c93a31d3ce081952048e5bd04
LeGioNPsyQ <65556998+LeGioNPsyQ@users.noreply.github.com>
2023-02-02 07:18:39 +0100

    Add files via upload

---
+++ Games/Rebirth Champion X.lua
@@ -0,0 +1,4 @@
+--╔════════════════════╗
+--║ Rebirth Champion X ║
+--╚════════════════════╝
+loadstring(game:HttpGet("https://raw.githubusercontent.com/KrystekYTpv/Scripts/main/RebirthChampionsX.lua", true))()
