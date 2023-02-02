7e1855774d43598588f1682cd2db42effcb96ccb
LeGioNPsyQ <65556998+LeGioNPsyQ@users.noreply.github.com>
2023-02-02 07:22:03 +0100

    Update LeGioN-Hub.lua

--- LeGioN-Hub.lua
+++ LeGioN-Hub.lua
@@ -3,7 +3,8 @@ local Games = {
 	[10821317529] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/Pickaxe%20Mining%20Simulator.lua", -- Pickaxe Mining Simulator
 	[9264596435] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/Idle%20Heroes%20Simulator.lua", -- Idle Heroes Simulator
 	[10925589760] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/Merge%20Simulator.lua", -- Merge Simulator
-	[11445923563] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/One%20Fruit%20Simulator.lua" -- One Fruit Simulator
+	[11445923563] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/One%20Fruit%20Simulator.lua", -- One Fruit Simulator
+	[8540346411] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/Rebirth%20Champion%20X.lua" -- Rebirth Champions X
 }
 
 if Games[game.PlaceId] then
