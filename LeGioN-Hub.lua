2847f4b88e14d0f10bb4f074f4c668b7165551c3
LeGioNPsyQ <65556998+LeGioNPsyQ@users.noreply.github.com>
2023-02-06 17:29:34 +0100

    Update LeGioN-Hub.lua

--- LeGioN-Hub.lua
+++ LeGioN-Hub.lua
@@ -5,20 +5,19 @@ local Games = {
 	[10925589760] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/Merge%20Simulator.lua", -- Merge Simulator
 	[11445923563] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/One%20Fruit%20Simulator.lua", -- One Fruit Simulator
 	[8540346411] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/Rebirth%20Champion%20X.lua" -- Rebirth Champions X
---	[8540346411] = "https://raw.githubusercontent.com/KrystekYTpv/Scripts/main/RebirthChampionsX.lua", -- Rebirth Chamption X
 
 }
 
 if Games[game.PlaceId] then
 	game.StarterGui:SetCore("SendNotification", {
-	    Title = "LeGioN Hub Notify";
+	    Title = "[LeGioN Hub] Notify";
 	    Text = "Script is loaded";
 	    Duration = 10;
     })
 	loadstring(game:HttpGet(Games[game.PlaceId]))()
 else
 	game.StarterGui:SetCore("SendNotification", {
-	    Title = "LeGioN Hub Notify";
+	    Title = "[LeGioN Hub] Notify";
 	    Text = "Game not supported";
 	    Duration = 10;
     })
