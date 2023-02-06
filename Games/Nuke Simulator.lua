f911a29b3cb487a42607b98b41bc7a63796627a0
LeGioNPsyQ <65556998+LeGioNPsyQ@users.noreply.github.com>
2023-02-06 20:23:47 +0100

    Update Nuke Simulator.lua

--- Games/Nuke Simulator.lua
+++ Games/Nuke Simulator.lua
@@ -2,11 +2,11 @@
 --║ Nuke Simulator ║
 --╚════════════════╝
 -- Collect All Drops
-loadstring(game:HttpGet("https://raw.githubusercontent.com/eufoni02/emperor/main/autocollectnukesim.lua"))()
+--loadstring(game:HttpGet("https://raw.githubusercontent.com/eufoni02/emperor/main/autocollectnukesim.lua"))()
 --loadstring(game:HttpGet('https://raw.githubusercontent.com/KodaLINEN/NukeSim/main/Main'))()
 
 -- Alpha Hub (Beste bis jz)
---loadstring(game:HttpGet("https://raw.githubusercontent.com/kimbenner/script/main/alphascript"))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/kimbenner/script/main/alphascript"))()
 
 -- Gems Stand vorm Schlafen gehen
 -- 30,179 Gems
