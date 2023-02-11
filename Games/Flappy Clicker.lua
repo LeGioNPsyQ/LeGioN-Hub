90dcf194885273b167a8481852150c7ecc6f3b6b
LeGioNPsyQ <65556998+LeGioNPsyQ@users.noreply.github.com>
2023-02-11 06:41:38 +0100

    Add files via upload

---
+++ Games/Flappy Clicker.lua
@@ -0,0 +1,7 @@
+--╔════════════════╗
+--║ Flappy Clicker ║
+--╚════════════════╝
+loadstring(game:HttpGet("https://raw.githubusercontent.com/ToraIsMe2/ToraIsMe2/main/0flappy", true))()
+
+-- NoobHub
+--loadstring(game:HttpGet(('https://raw.githubusercontent.com/danixbo/NoobHub/main/NoobHub')))()
