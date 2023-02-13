9872433b21b05616283336f329987c5308f4b055
LeGioNPsyQ <65556998+LeGioNPsyQ@users.noreply.github.com>
2023-02-13 19:02:34 +0100

    Update LeGioN-Hub.lua

--- LeGioN-Hub.lua
+++ LeGioN-Hub.lua
@@ -1,13 +1,13 @@
 local Games = {
-	[8540346411] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/Rebirth%20Champion%20X.lua", -- Rebirth Champions X
-	[10821317529] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/Pickaxe%20Mining%20Simulator.lua", -- Pickaxe Mining Simulator
+	[8540346411] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/RebirthChampionX.lua", -- Rebirth Champions X
+	[10821317529] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/PickaxeMiningSimulator.lua", -- Pickaxe Mining Simulator
 
-	[10404327868] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/Timber%20Champions.lua", -- Timber Champions
-	[9264596435] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/Idle%20Heroes%20Simulator.lua", -- Idle Heroes Simulator
-	[10925589760] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/Merge%20Simulator.lua", -- Merge Simulator
-	[11445923563] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/One%20Fruit%20Simulator.lua", -- One Fruit Simulator
+	[10404327868] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/TimberChampions.lua", -- Timber Champions
+	[9264596435] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/IdleHeroesSimulator.lua", -- Idle Heroes Simulator
+	[10925589760] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/MergeSimulator.lua", -- Merge Simulator
+	[11445923563] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/OneFruitSimulator.lua", -- One Fruit Simulator
 	
-	[11599913094] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/Nuke%20Simulator.lua", -- Nuke Simulator
+	[11599913094] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/NukeSimulator.lua", -- Nuke Simulator
 	[2512643572] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/BubbleGumSim.lua", -- Bubble Gum Simulator
 	[11620947043] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/FlappyClicker.lua", -- Flappy Clicker
 	[11746859781] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/BubbleGumClicker.lua" -- Bubble Gum Clicker
