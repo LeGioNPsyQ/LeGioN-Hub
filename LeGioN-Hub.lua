17609ac8d5c29232563ebcc4063a0f40fe161d08
LeGioNPsyQ <65556998+LeGioNPsyQ@users.noreply.github.com>
2023-03-12 00:19:02 +0100

    Update LeGioN-Hub.lua

--- LeGioN-Hub.lua
+++ LeGioN-Hub.lua
@@ -1,27 +1,35 @@
 local Games = {
-	[8540346411] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/RebirthChampionX.lua", -- Rebirth Champions X
-	[10821317529] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/PickaxeMiningSimulator.lua", -- Pickaxe Mining Simulator
+--  	GAME ID      	GAME SCRIPT LINK
+--[[	FAVORITEN	]]
+	[8540346411] 	= "https://raw.githubusercontent.com/KrystekYTpv/Scripts/main/RebirthChampionsX.lua", 						-- Rebirth Chamption X
+	[10404327868] 	= "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/Timber%20Champions.lua", 		-- Timber Champions
+	[10821317529] 	= "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/Pickaxe%20Mining%20Simulator.lua", 	-- Pickaxe Mining Simulator
 
-	[10404327868] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/TimberChampions.lua", -- Timber Champions
-	[9264596435] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/IdleHeroesSimulator.lua", -- Idle Heroes Simulator
-	[10925589760] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/MergeSimulator.lua", -- Merge Simulator
-	[11445923563] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/OneFruitSimulator.lua", -- One Fruit Simulator
-	
-	[11599913094] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/NukeSimulator.lua", -- Nuke Simulator
-	[2512643572] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/BubbleGumSim.lua", -- Bubble Gum Simulator
-	[11620947043] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/FlappyClicker.lua", -- Flappy Clicker
-	[11746859781] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/BubbleGumClicker.lua" -- Bubble Gum Clicker
+	[11448052802] 	= "https://raw.githubusercontent.com/danixbo/NoobHub/main/NoobHub", 			-- Pet Rift
+	[11542692507] 	= "https://raw.githubusercontent.com/bunnynwy/games/main/petlegends2", 			-- Pet Legends 2!
+	[11599913094] 	= "https://raw.githubusercontent.com/eufoni02/emperor/main/autocollectnukesim.lua", 	-- Nuke Simulator
+	[11620947043] 	= "https://raw.githubusercontent.com/danixbo/NoobHub/main/NoobHub", 			-- Flappy Clicker
+	[11746859781] 	= "https://raw.githubusercontent.com/danixbo/NoobHub/main/NoobHub", 			-- Bubble Gum Clicker
+
+--[[	Andere GAMES	]]
+
+	[9264596435] 	= "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/Idle%20Heroes%20Simulator.lua", 	-- Idle Heroes Simulator
+	[10033287610] 	= "https://raw.githubusercontent.com/Pikaruru/Scripts/main/smasherman.lua", 							-- Smasherman Simulator
+	[10925589760] 	= "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/Merge%20Simulator.lua", 		-- Merge Simulator
+	[11254130090] 	= "https://raw.githubusercontent.com/Pikaruru/Scripts/main/pettappingsimulator.lua" 						-- Strongest Tapper Simulator
+	[11445923563] 	= "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/One%20Fruit%20Simulator.lua" 	-- One Fruit Simulator
 }
+
 if Games[game.PlaceId] then
 	game.StarterGui:SetCore("SendNotification", {
-	    Title = "[LeGioN Hub] Notify";
+	    Title = "LeGioN Hub Notify";
 	    Text = "Script is loaded";
 	    Duration = 10;
     })
 	loadstring(game:HttpGet(Games[game.PlaceId]))()
 else
 	game.StarterGui:SetCore("SendNotification", {
-	    Title = "[LeGioN Hub] Notify";
+	    Title = "LeGioN Hub Notify";
 	    Text = "Game not supported";
 	    Duration = 10;
     })
