344f28fb89f46f473abd98891687898596054b15
LeGioNPsyQ <65556998+LeGioNPsyQ@users.noreply.github.com>
2023-04-25 23:43:26 +0200

    LeGioN-Hub.lua

--- LeGioN-Hub.lua
+++ LeGioN-Hub.lua
@@ -1,25 +1,23 @@
 local Games = {
---  	GAME ID      	GAME SCRIPT LINK
---[[	FAVORITEN	]]
-	[8540346411] 	= "https://raw.githubusercontent.com/KrystekYTpv/Scripts/main/RebirthChampionsX.lua", 						-- Rebirth Chamption X
-	[8834250772] 	= "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/AttackSimulator.lua" 					-- Attack Simulator
-	[10404327868] 	= "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/Timber%20Champions.lua", 		-- Timber Champions
-	[10821317529] 	= "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/Pickaxe%20Mining%20Simulator.lua", -- Pickaxe Mining Simulator
+	[8540346411] = "https://raw.githubusercontent.com/KrystekYTpv/Scripts/main/RebirthChampionsX.lua", 						-- Rebirth Chamption X
+	[8834250772] = "https://raw.githubusercontent.com/LeGioNPsyQ/LeGioN-Hub/main/Games/AttackSimulator.lua" 					-- Attack Simulator
+	[10404327868] = "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/Timber%20Champions.lua", 		-- Timber Champions
+	[10821317529] = "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/Pickaxe%20Mining%20Simulator.lua", -- Pickaxe Mining Simulator
 
-	[11448052802] 	= "https://raw.githubusercontent.com/danixbo/NoobHub/main/NoobHub", 			-- Pet Rift
-	[11542692507] 	= "https://raw.githubusercontent.com/bunnynwy/games/main/petlegends2", 			-- Pet Legends 2!
-	[11599913094] 	= "https://raw.githubusercontent.com/eufoni02/emperor/main/autocollectnukesim.lua", 	-- Nuke Simulator
-	[11620947043] 	= "https://raw.githubusercontent.com/danixbo/NoobHub/main/NoobHub", 			-- Flappy Clicker
-	[11746859781] 	= "https://raw.githubusercontent.com/danixbo/NoobHub/main/NoobHub", 			-- Bubble Gum Clicker
+	[11448052802] = "https://raw.githubusercontent.com/danixbo/NoobHub/main/NoobHub", 			-- Pet Rift
+	[11542692507] = "https://raw.githubusercontent.com/bunnynwy/games/main/petlegends2", 			-- Pet Legends 2!
+	[11599913094] = "https://raw.githubusercontent.com/eufoni02/emperor/main/autocollectnukesim.lua", 	-- Nuke Simulator
+	[11620947043] = "https://raw.githubusercontent.com/danixbo/NoobHub/main/NoobHub", 			-- Flappy Clicker
+	[11746859781] = "https://raw.githubusercontent.com/danixbo/NoobHub/main/NoobHub", 			-- Bubble Gum Clicker
 
 --[[	Andere GAMES	]]
 
-	[9264596435] 	= "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/Idle%20Heroes%20Simulator.lua", 	-- Idle Heroes Simulator
-	[10033287610] 	= "https://raw.githubusercontent.com/Pikaruru/Scripts/main/smasherman.lua", 							-- Smasherman Simulator
-	[10674429038] = "https://raw.githubusercontent.com/Pikaruru/Scripts/main/pickaxesim.lua",							-- Pickaxe Simulator
-	[10925589760] 	= "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/Merge%20Simulator.lua", 		-- Merge Simulator
-	[11254130090] 	= "https://raw.githubusercontent.com/Pikaruru/Scripts/main/pettappingsimulator.lua", 						-- Strongest Tapper Simulator
-	[11445923563] 	= "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/One%20Fruit%20Simulator.lua" 	-- One Fruit Simulator
+[9264596435] 	= "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/Idle%20Heroes%20Simulator.lua", 	-- Idle Heroes Simulator
+[10033287610] 	= "https://raw.githubusercontent.com/Pikaruru/Scripts/main/smasherman.lua", 							-- Smasherman Simulator
+[10674429038] = "https://raw.githubusercontent.com/Pikaruru/Scripts/main/pickaxesim.lua",							-- Pickaxe Simulator
+[10925589760] 	= "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/Merge%20Simulator.lua", 		-- Merge Simulator
+[11254130090] 	= "https://raw.githubusercontent.com/Pikaruru/Scripts/main/pettappingsimulator.lua", 						-- Strongest Tapper Simulator
+[11445923563] 	= "https://raw.githubusercontent.com/LeGioNPsyQ/Scripts/main/DevilNetWork%20X%20Scripts/Games/One%20Fruit%20Simulator.lua" 	-- One Fruit Simulator
 }
 
 if Games[game.PlaceId] then
